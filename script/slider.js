d619c94125cb88a3612aa5ce4dce7e049f8e282d
Anna Babay <kot-vmeshke.4@gmail.com>
2022-04-19 18:12:33 +0200

    feat: add slider

---
+++ script/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+    slidesToShow: 3,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: '60px',
+  });
+});
